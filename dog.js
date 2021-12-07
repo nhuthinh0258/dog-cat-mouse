02b922d4d701e7c49de65d32f03310486a5e43df
nhuthinh0258 <nhuthinh0258@gmail.com>
2021-12-07 14:51:26 +0700

    sayHi method for dog

--- dog.js
+++ dog.js
@@ -1,7 +1,16 @@
-function dog(){
+var chalk=require('chalk');
+
+function dog(name){
+    this.name=name;
     this.stomatch=[];
-}
+;}
 
 dog.prototype.eat=function(cat){
     this.stomatch.push(cat);
-}
+};
+
+dog.prototype.sayHi=function(){
+    console.log('Hello, My name is' + chalk.blue(this.name))
+};
+
+module.exports=dog;
---
+++ index.js
@@ -0,0 +1,5 @@
+var dog= require('./dog');
+
+var dog= new dog('Tom');
+
+dog.sayHi();
